1cb1d43c4c5eea470d5b397800f191e1718f2847
OlhaVoloshenko <voloshenko.olya@gmail.com>
2022-10-27 14:27:37 +0300

    111

--- controllers/contacts/listContacts.js
+++
@@ -1,8 +0,0 @@
-const contacts = require("../../models/contacts");
-
-const listContacts = async (_, res) => {
-  const result = await contacts.listContacts();
-  res.json(result);
-};
-
-module.exports = listContacts;
