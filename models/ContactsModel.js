7f7248b50fea284874bfb79313ebc30a22d8b16e
OlhaVoloshenko <voloshenko.olya@gmail.com>
2022-10-29 20:55:17 +0300

    Add actual files

--- models/ContactsModel.js
+++ models/ContactsModel.js
@@ -54,25 +54,6 @@ const addSchema = Joi.object({
 const schemas = {
   addSchema,
 };
-// const User = db.model("user", userSchema);
-// const user = new User();
-// let error;
-
-// user.phone = '555.0123';
-// error = user.validateSync();
-// assert.equal(error.errors['phone'].message,
-//   '555.0123 is not a valid phone number!');
-
-// user.phone = '';
-// error = user.validateSync();
-// assert.equal(error.errors['phone'].message,
-//   'User phone number required');
-
-// user.phone = '201-555-0123';
-// // Validation succeeds! Phone number is defined
-// // and fits `DDD-DDD-DDDD`
-// error = user.validateSync();
-// assert.equal(error, null);
 const ContactModel = model("Contact", ContactSchema);
 
 module.exports = {
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,51 +1,25 @@
-// const express = require("express");
-// const ctrl = require("../../controllers/contacts");
-// const { ctrlWrapper } = require("../../helpers");
-// const { validateBody } = require("../../middlewares");
-// const contactsSchema = require("../../schemas");
-
-// const router = express.Router();
-
-// router.get("/", ctrlWrapper(ctrl.listContacts));
-
-// router.get("/:contactId", ctrlWrapper(ctrl.getContactById));
-
-// router.post(
-//   "/",
-//   validateBody(contactsSchema.addSchema),
-//   ctrlWrapper(ctrl.addContact)
-// );
-
-// router.put(
-//   "/:contactId",
-//   validateBody(contactsSchema.addSchema),
-//   ctrlWrapper(ctrl.updateById)
-// );
-
-// router.delete("/:contactId", ctrlWrapper(ctrl.removeContact));
-
-// module.exports = router;
 const express = require("express");
 const ctrl = require("../../controllers/contacts");
 const { ctrlWrapper } = require("../../helpers");
-const { validateBody } = require("../../middlewares");
-const { addSchema } = require("../../schemas/contactsSchema");
+const { validateBody, isValidId } = require("../../middlewares");
+const { schemas } = require("../../models/ContactsModel");
 
 const router = express.Router();
 
 router.get("/", ctrlWrapper(ctrl.getAll));
 
-router.get("/:contactId", ctrlWrapper(ctrl.getById));
+router.get("/:contactId", isValidId, ctrlWrapper(ctrl.getById));
 
+router.post("/", validateBody(schemas.addSchema), ctrlWrapper(ctrl.add));
 // router.post("/", ctrlWrapper(ctrl.add));
-router.post("/", validateBody(addSchema), ctrlWrapper(ctrl.add));
 
 router.put(
   "/:contactId",
-  validateBody(addSchema),
+  isValidId,
+  validateBody(schemas.addSchema),
   ctrlWrapper(ctrl.updateById)
 );
 
-router.delete("/:contactId", ctrlWrapper(ctrl.removeById));
+router.delete("/:contactId", isValidId, ctrlWrapper(ctrl.removeById));
 
 module.exports = router;
