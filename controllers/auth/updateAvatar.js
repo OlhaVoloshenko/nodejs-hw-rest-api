59652b33216545897b5c4a8a9cebc756c5d23d1b
OlhaVoloshenko <voloshenko.olya@gmail.com>
2022-11-04 08:28:35 +0200

    Add very importent code

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,15 +1,23 @@
 const fs = require("fs/promises");
 const path = require("path");
 const { UserModel } = require("../../models/UserModel");
+const Jimp = require("jimp");
 
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
 const updateAvatar = async (req, res) => {
+  const { _id } = req.user;
+  const { path: tempUpload, originalname } = req.file;
   try {
-    const { _id } = req.user;
-    const { path: tempUpload, originalname } = req.file;
+    const image = await Jimp.read(tempUpload);
+    await image.resize(250, 250).write(tempUpload);
+
     const extention = originalname.split(".").pop();
+
     const filename = `${_id}.${extention}`;
+    // Jimp.read("./path/to/image.jpg")
+    //   .then((image) => {})
+    //   .catch((err) => {});
     const resultUpload = path.join(avatarsDir, filename);
     await fs.rename(tempUpload, resultUpload);
     const avatarURL = path.join("avatars", filename);
